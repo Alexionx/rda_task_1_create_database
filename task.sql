d1da2babe71e7086011329ae7436d56113e86442
KempachiDZ <alexmelnyk48370@gmail.com>
2025-02-20 20:37:35 +0200

    Solve task-v2

--- task.sql
+++ task.sql
@@ -17,7 +17,7 @@ CREATE table Customers (
     FirstName varchar(50),
     LastName varchar(50),
     Email varchar(50),
-    Address varchar(100)
+    Address varchar(100),
     PRIMARY KEY(ID)
 );
 
@@ -25,7 +25,7 @@ CREATE table Orders (
     ID INT AUTO_INCREMENT,
     CustomerID INT,
     Date Date,
-    FOREIGN KEY Products(ID) ON DELETE SET NULL,
+    FOREIGN KEY CustomerID REFERENCES Customers(ID) ON DELETE SET NULL,
     PRIMARY KEY (ID)
 );
 
@@ -33,6 +33,6 @@ CREATE table OrderItems (
     ID INT AUTO_INCREMENT, 
     OrderID INT,
     ProductID INT,
-    FOREIGN KEY Orders(ID) ON DELETE SET NULL,
-    FOREIGN KEY Products(ID) ON DELETE SET NULL
+    FOREIGN KEY OrderID REFERENCES Orders(ID) ON DELETE SET NULL,
+    FOREIGN KEY ProductID REFERENCES Products(ID) ON DELETE SET NULL
 );
